3ad2b7695fcc0ae6f1aa19bf67361689aacec4be
mxlle <almut.m.f@gmail.com>
2021-12-24 16:15:17 +0100

    Allow 2 player

--- ui/src/game-logic/gameConsts.ts
+++ ui/src/game-logic/gameConsts.ts
@@ -19,7 +19,7 @@ export const masterFaker = '🥸';
 export const painter = Role.PAINTER;
 export const buyer = Role.BUYER;
 
-export const minNumPlayers = 3;
+export const minNumPlayers = 2;
 export const themesPerRound = 2;
 export const minDemand = 2;
 export const maxDemand = 7;
