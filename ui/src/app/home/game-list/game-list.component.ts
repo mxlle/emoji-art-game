ba9cbe2c2463d3a4fb159f49a725c51cd2c7d636
mxlle <almut.m.f@gmail.com>
2022-01-16 14:18:45 +0100

    Game list: limit length

--- ui/src/app/home/game-list/game-list.component.ts
+++ ui/src/app/home/game-list/game-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, TrackByFunction } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output, TrackByFunction } from '@angular/core';
 import { GameInfo, Player } from '../../../game-logic/game';
 import { trackByObjectId } from '../../util/ui-helpers';
 import { getCurrentUserId } from '../../../data/functions';
@@ -11,15 +11,28 @@ import { getPhaseEmojis, pointsEmoji } from '../../../game-logic/gameConsts';
   styleUrls: ['./game-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GameListComponent {
+export class GameListComponent implements OnInit {
   @Input() label: string = '';
   @Input() games: GameInfo[] = [];
+  @Input() initialLimit: number = 5;
   @Output() delete: EventEmitter<string> = new EventEmitter<string>();
+
+  limit: number = this.initialLimit;
 
   readonly trackByGameId: TrackByFunction<GameInfo> = trackByObjectId;
   readonly getPhaseEmojis = getPhaseEmojis;
   readonly currentUserId: string = getCurrentUserId();
   readonly pointsEmoji: string = pointsEmoji;
+
+  private readonly limitStep: number = 5;
+
+  ngOnInit() {
+    this.limit = this.initialLimit;
+  }
+
+  increaseLimit() {
+    this.limit += this.limitStep;
+  }
 
   getPlayersString(game: GameInfo): string {
     return game.players.map((pl: Player) => pl.name).join(', ') || '-';
